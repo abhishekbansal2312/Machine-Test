b81d64d9bcf4daf74c99550284248b49dd83872f
Abhishek Bansal <abhishekbansal2312@gmail.com>
2025-03-20 21:01:48 +0530

    Refactor server package to update dependencies

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ dotenv.config();
 
 // Initialize express app
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5004;
 
 // Middleware
 app.use(cors());
